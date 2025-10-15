4dbc5fa9551f1936e4cd3e2686c007f61cfd8cff
Taylor Clark Jones <taylorclarkjones@gmail.com>
2025-10-15 08:36:57 -0400

    Remove firebase-config from repo

--- firebase-config.js
+++
@@ -1,9 +0,0 @@
-// firebase-config.js
-export const firebaseConfig = {
-  apiKey: "AIzaSyAldYSp37LZO31XkGc4F1xhnQ9bpBXLU6Q",
-  authDomain: "williams-reunion.firebaseapp.com",
-  projectId: "williams-reunion",
-  storageBucket: "williams-reunion.firebasestorage.app",
-  messagingSenderId: "1053815265185",
-  appId: "1:1053815265185:web:050db056f81ab9d2d81de0"
-};
