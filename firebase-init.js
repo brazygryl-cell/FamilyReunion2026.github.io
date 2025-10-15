049dd1597816473463ef4f8fef7a6ca492db5f09
brazygryl-cell <brazygryl@gmail.com>
2025-10-15 08:16:36 -0400

    Update firebase-init.js

--- firebase-init.js
+++ firebase-init.js
@@ -1,10 +1,7 @@
-// firebase-init.js
 import { firebaseConfig } from './firebase-config.js';
 
-// These scripts need to be loaded in HTML (compat)
 const app = firebase.initializeApp(firebaseConfig);
 const auth = firebase.auth();
 const db = firebase.firestore();
 
-// Export so other scripts can use
 export { auth, db };
