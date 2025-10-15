5005aacc51eb8ab383f1a8b1ba485d4ce7bd56f1
brazygryl-cell <brazygryl@gmail.com>
2025-10-15 07:33:47 -0400

    Create firebase-init.js

---
+++ firebase-init.js
@@ -0,0 +1,13 @@
+// firebase-init.js
+const firebaseConfig = {
+  apiKey: "AIzaSyAldYSp37LZO31XkGc4F1xhnQ9bpBXLU6Q",
+  authDomain: "williams-reunion.firebaseapp.com",
+  projectId: "williams-reunion",
+  storageBucket: "williams-reunion.firebasestorage.app",
+  messagingSenderId: "1053815265185",
+  appId: "1:1053815265185:web:050db056f81ab9d2d81de0"
+};
+
+const app = firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
+const db = firebase.firestore();
