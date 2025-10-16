fb4dbbc226aa025bbb0d95be2990e6a566f52e52
brazygryl-cell <brazygryl@gmail.com>
2025-10-16 18:10:33 -0400

    Update firebase-init.js

--- firebase-init.js
+++ firebase-init.js
@@ -1,10 +1,7 @@
 // firebase-init.js
 import { firebaseConfig } from './firebase-config.js';
 
-if (!firebase.apps.length) {
-  firebase.initializeApp(firebaseConfig);
-}
-
+const app = firebase.initializeApp(firebaseConfig);
 const auth = firebase.auth();
 const db = firebase.firestore();
 
