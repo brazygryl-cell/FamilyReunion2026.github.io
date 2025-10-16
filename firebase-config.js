db852f647755d6df735798b5f2da62e5d2979e1e
brazygryl-cell <brazygryl@gmail.com>
2025-10-16 18:18:33 -0400

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,10 @@
 // firebase-config.js
+// firebase-config.js
 export const firebaseConfig = {
   apiKey: "AIzaSyAldYSp37LZO31XkGc4F1xhnQ9bpBXLU6Q",
   authDomain: "williams-reunion.firebaseapp.com",
   projectId: "williams-reunion",
-  storageBucket: "williams-reunion.appspot.com,
+  storageBucket: "williams-reunion.appspot.com",
   messagingSenderId: "1053815265185",
   appId: "1:1053815265185:web:050db056f81ab9d2d81de0"
 };
