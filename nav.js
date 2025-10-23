6a5a466d365a6f9c4e28aee5ca278bf238fb04af
brazygryl-cell <brazygryl@gmail.com>
2025-10-22 21:50:19 -0400

    Update nav.js

--- nav.js
+++ nav.js
@@ -11,8 +11,10 @@ export function loadNavbar() {
       <nav class="nav-links">
         <a href="index.html">Home</a>
         <a href="event-details.html">Event Details</a>
+        <a href="lodging-travel.html">Lodging</a>
         <a href="forum.html">Forum</a>
-        <a href="contact.html">Contact</a>
+        <a href="photo-gallery.html">Photo Gallery</a>
+        <a href="family-history.html">Family History</a>
         <a href="#" id="logoutBtn" style="color:#c33;font-weight:600">Logout</a>
       </nav>
     `;
