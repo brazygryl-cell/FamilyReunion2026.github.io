8b3f9edb4d40c0408cd0542815aa89a90dff2f28
brazygryl-cell <brazygryl@gmail.com>
2025-10-26 10:35:37 -0400

    Delete env.js

--- env.js
+++
@@ -1,8 +0,0 @@
-window.env = {
-  apiKey: "AIzaSyAldYSp37LZO31XkGc4F1xhnQ9bpBXLU6Q",
-  authDomain: "williams-reunion.firebaseapp.com",
-  projectId: "williams-reunion",
-  storageBucket: "williams-reunion.firebasestorage.app",
-  messagingSenderId: "1053815265185",
-  appId: "1:1053815265185:web:050db056f81ab9d2d81de0"
-};
