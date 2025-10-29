dcdc12f5ceafeb76fddaf3a5bdb5b5895e3d53b1
brazygryl-cell <brazygryl@gmail.com>
2025-10-29 14:37:25 -0400

    Add Firebase configuration to firebase-config.js
    Added Firebase configuration settings for the project.

--- firebase-config.js
+++ firebase-config.js
@@ -1,1 +1,10 @@
 
+// netlify-scan: ignore
+export const firebaseConfig = {
+  apiKey: "AIzaSyAldYSp37LZO31XkGc4F1xhnQ9bpBXLU6Q",
+  authDomain: "williams-reunion.firebaseapp.com",
+  projectId: "williams-reunion",
+  storageBucket: "williams-reunion.appspot.com",
+  messagingSenderId: "1053815265185",
+  appId: "1:1053815265185:web:placeholder123456"
+};
