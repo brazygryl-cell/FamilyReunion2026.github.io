fb47dece27c1d89c82d9ad881bd11b349526f338
brazygryl-cell <brazygryl@gmail.com>
2025-10-30 17:57:33 -0400

    Update Firebase config with hardcoded values

--- netlify/functions/firebase-config.js
+++ netlify/functions/firebase-config.js
@@ -3,9 +3,9 @@ export async function handler() {
     statusCode: 200,
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({
-      apiKey: process.env.FIREBASE_API_KEY,
-      authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-      projectId: process.env.FIREBASE_PROJECT_ID
+      apiKey: process.env.FIREBASE_API_KEY,  // pulled from Netlify env
+      authDomain: "williams-reunion.firebaseapp.com",
+      projectId: "williams-reunion"
     })
   };
 }
