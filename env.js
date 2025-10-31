94ca432e2c00b89be6a4ae0b3690e3c2d3575891
brazygryl-cell <brazygryl@gmail.com>
2025-10-31 16:32:46 -0400

    Update Firebase configuration in env.js

--- env.js
+++ env.js
@@ -1,3 +1,5 @@
 window._env_ = {
-  FIREBASE_API_KEY: "${FIREBASE_API_KEY}"
+  FIREBASE_API_KEY: "AIzaSyAldYSp37LZO31XkGc4F1xhnQ9bpBXLU6Q",
+  FIREBASE_AUTH_DOMAIN: "williams-reunion.firebaseapp.com",
+  FIREBASE_PROJECT_ID: "williams-reunion"
 };
