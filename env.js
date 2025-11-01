4eb6d5d0d2e5e29484302dd3687210b5f941d73a
brazygryl-cell <brazygryl@gmail.com>
2025-11-01 12:11:16 -0400

    Update env.js

--- env.js
+++ env.js
@@ -3,4 +3,4 @@ window.ENV = {
   FIREBASE_API_KEY: "AIzaSyAldYSp37LZO31XkGc4F1xhnQ9bpBXLU6Q",
   FIREBASE_AUTH_DOMAIN: "williams-reunion.firebaseapp.com",
   FIREBASE_PROJECT_ID: "williams-reunion"
-};
+}; // comment
