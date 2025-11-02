0c1256db0cb54992098108a7193f192db7f319fa
brazygryl-cell <brazygryl@gmail.com>
2025-11-02 10:27:54 -0500

    Delete env.js

--- env.js
+++
@@ -1,5 +0,0 @@
-window.ENV = {
- FIREBASE_API_KEY: "AIzaSyAldYSp37LZO31XkG4F1xhnQ9bpBXLU6Q",
- FIREBASE_AUTH_DOMAIN: "williams-reunion.firebaseapp.com",
- FIREBASE_PROJECT_ID: "williams-reunion"
-};
