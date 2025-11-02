968dbc8d023b965442471588a50e3ec552f8f389
brazygryl-cell <brazygryl@gmail.com>
2025-11-02 12:35:50 -0500

    Create env.js

---
+++ env.js
@@ -0,0 +1,5 @@
+window.ENV = {
+ FIREBASE_API_KEY: "AIzaSyAldYSp37LZO31XkG4F1xhnQ9bpBXLU6Q",
+ FIREBASE_AUTH_DOMAIN: "williams-reunion.firebaseapp.com",
+ FIREBASE_PROJECT_ID: "williams-reunion"
+};
