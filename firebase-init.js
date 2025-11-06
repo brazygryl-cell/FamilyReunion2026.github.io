9d04b667e092dbba9eb958c1b27851a8ddecc615
brazygryl-cell <brazygryl@gmail.com>
2025-11-06 07:52:23 -0500

    Update firebase-init.js

--- firebase-init.js
+++ firebase-init.js
@@ -1,15 +1,9 @@
 // firebase-init.js
+const cfg = window.__FIREBASE_CONFIG__;
+
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-firestore.js";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyAldYSp37LZO31XkGc4F1xhnQ9bpBXLU6Q",
-  authDomain: "williams-reunion.firebaseapp.com",
-  projectId: "williams-reunion",
-  storageBucket: "williams-reunion.appspot.com",
-  messagingSenderId: "1053815265185",
-  appId: "1:1053815265185:web:YOUR_APP_ID_HERE"
-};
-
-const app = initializeApp(firebaseConfig);
+// Initialize Firebase with the config injected at build time
+const app = initializeApp(cfg);
 export const db = getFirestore(app);
